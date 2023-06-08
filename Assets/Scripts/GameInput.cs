4e9e8851b970d4ee6565e065f2cfcdef907d9bc9
Gerard Baholli <gerardbaholli@live.it>
2023-06-08 15:49:52 +0200

    Add lessons up to h2 min04

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one GameInput! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        return inputVector.normalized;
+    }
+
+}
