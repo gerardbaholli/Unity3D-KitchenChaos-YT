ed353993406af1fbed2b39ec1ba5b6329b06e6da
Gerard Baholli <gerardbaholli@live.it>
2023-06-21 14:29:55 +0200

    Add lessons up to h8 min30

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -23,6 +23,8 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
+
 
     private void Awake()
     {
@@ -85,6 +87,8 @@ public class DeliveryManager : MonoBehaviour
                 {
                     // Player delivered the correct recipe!
 
+                    successfulRecipesAmount++;
+
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
@@ -102,6 +106,11 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 
 }
