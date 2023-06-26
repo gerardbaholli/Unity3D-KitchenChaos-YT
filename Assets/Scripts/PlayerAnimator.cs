83880582502418f9e9cd4d2d0601bc6af551643e
Gerard Baholli <gerardbaholli@live.it>
2023-06-26 23:04:49 +0200

    Add Multiplayer Lecture 3

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
+public class PlayerAnimator : NetworkBehaviour
 {
+
+
     private const string IS_WALKING = "IsWalking";
 
-    [SerializeField] Player player;
+
+    [SerializeField] private Player player;
+
 
     private Animator animator;
+
 
     private void Awake()
     {
@@ -17,6 +23,11 @@ public class PlayerAnimator : MonoBehaviour
 
     private void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
+
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
 
