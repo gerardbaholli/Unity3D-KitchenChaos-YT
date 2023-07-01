9e8d1e2f08866ded45c3a022ad748fc4d0534c97
Gerard Baholli <gerardbaholli@live.it>
2023-07-01 19:39:31 +0200

    Add Multiplayer Lecture 16

--- Assets/Scripts/UI/HostDisconnectUI.cs
+++ Assets/Scripts/UI/HostDisconnectUI.cs
@@ -1,58 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameOverUI : MonoBehaviour
+public class HostDisconnectUI : MonoBehaviour
 {
 
 
-    [SerializeField] private TextMeshProUGUI recipesDeliveredText;
     [SerializeField] private Button playAgainButton;
 
 
-    private void Awake()
+    private void Start()
     {
+        NetworkManager.Singleton.OnClientDisconnectCallback += NetworkManager_OnClientDisconnectCallback;
+
         playAgainButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.Shutdown();
             Loader.Load(Loader.Scene.MainMenuScene);
         });
-    }
-
-    private void Start()
-    {
-        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
 
         Hide();
     }
 
-    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    private void NetworkManager_OnClientDisconnectCallback(ulong clientId)
     {
-        if (KitchenGameManager.Instance.IsGameOver())
+        if (clientId == NetworkManager.ServerClientId)
         {
+            // Server is shutting down
             Show();
-
-            recipesDeliveredText.text = DeliveryManager.Instance.GetSuccessfulRecipesAmount().ToString();
-        }
-        else
-        {
-            Hide();
         }
     }
 
     private void Show()
     {
         gameObject.SetActive(true);
-        playAgainButton.Select();
     }
 
     private void Hide()
     {
         gameObject.SetActive(false);
     }
-
 
 }
